ba51362a19031937fe32e5504f80f67d50dbfd68
lawrencemboya <lawrencemboya@gmail.com>
2024-08-22 07:21:28 -0400

    adds MatchInput class with all fields, getters and setters

---
+++ src/main/java/com/dashboard/ipl_dashboard/data/MatchInput.java
@@ -0,0 +1,184 @@
+package com.dashboard.ipl_dashboard.data;
+
+public class MatchInput {
+    private String Match_id;
+    private String season;
+    private String city;
+    private String date;
+    private String team1;
+    private String team2;
+    private String toss_win;
+    private String toss_decision;
+    private String result;
+    private String dl_applied;
+    private String winner;
+    private String win_by_runs;
+    private String win_by_wickets;
+    private String player_of_the_match;
+    private String venue;
+    private String match_refree;
+    private String reserve_umpire;
+    private String tv_umpire;
+    private String umpire1;
+    private String umpire2;
+
+    public String getMatch_id() {
+        return Match_id;
+    }
+
+    public void setMatch_id(String match_id) {
+        Match_id = match_id;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+
+    public String getToss_win() {
+        return toss_win;
+    }
+
+    public void setToss_win(String toss_win) {
+        this.toss_win = toss_win;
+    }
+
+    public String getToss_decision() {
+        return toss_decision;
+    }
+
+    public void setToss_decision(String toss_decision) {
+        this.toss_decision = toss_decision;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getDl_applied() {
+        return dl_applied;
+    }
+
+    public void setDl_applied(String dl_applied) {
+        this.dl_applied = dl_applied;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public String getWin_by_runs() {
+        return win_by_runs;
+    }
+
+    public void setWin_by_runs(String win_by_runs) {
+        this.win_by_runs = win_by_runs;
+    }
+
+    public String getWin_by_wickets() {
+        return win_by_wickets;
+    }
+
+    public void setWin_by_wickets(String win_by_wickets) {
+        this.win_by_wickets = win_by_wickets;
+    }
+
+    public String getPlayer_of_the_match() {
+        return player_of_the_match;
+    }
+
+    public void setPlayer_of_the_match(String player_of_the_match) {
+        this.player_of_the_match = player_of_the_match;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getMatch_refree() {
+        return match_refree;
+    }
+
+    public void setMatch_refree(String match_refree) {
+        this.match_refree = match_refree;
+    }
+
+    public String getReserve_umpire() {
+        return reserve_umpire;
+    }
+
+    public void setReserve_umpire(String reserve_umpire) {
+        this.reserve_umpire = reserve_umpire;
+    }
+
+    public String getTv_umpire() {
+        return tv_umpire;
+    }
+
+    public void setTv_umpire(String tv_umpire) {
+        this.tv_umpire = tv_umpire;
+    }
+
+    public String getUmpire1() {
+        return umpire1;
+    }
+
+    public void setUmpire1(String umpire1) {
+        this.umpire1 = umpire1;
+    }
+
+    public String getUmpire2() {
+        return umpire2;
+    }
+
+    public void setUmpire2(String umpire2) {
+        this.umpire2 = umpire2;
+    }
+}
